6dc65cf8b32d02b18853e7359498d3e38473a6da
Achmad Kumail Arif <ackuarif@gmail.com>
2023-04-28 10:16:20 +0700

    add logout feature

--- src/components/header/AppHeaderDropdown.js
+++ src/components/header/AppHeaderDropdown.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {
   CAvatar,
   CBadge,
@@ -24,8 +24,20 @@ import {
 import CIcon from '@coreui/icons-react'
 
 import avatar8 from './../../assets/images/avatars/8.jpg'
+import { putAccessToken } from 'src/utils/api'
+import { useNavigate } from 'react-router-dom'
+import UserContext from 'src/contexts/UserContext'
 
 const AppHeaderDropdown = () => {
+  const navigate = useNavigate()
+  const { authedUser, setAuthedUser } = useContext(UserContext)
+
+  const logoutSubmit = () => {
+    putAccessToken('')
+    setAuthedUser(null)
+    navigate('/login')
+  }
+
   return (
     <CDropdown variant="nav-item">
       <CDropdownToggle placement="bottom-end" className="py-0" caret={false}>
@@ -34,7 +46,7 @@ const AppHeaderDropdown = () => {
       <CDropdownMenu className="pt-0" placement="bottom-end">
         <CDropdownHeader className="bg-light fw-semibold py-2">Account</CDropdownHeader>
         <CDropdownDivider />
-        <CDropdownItem href="#">
+        <CDropdownItem href="javascript:void(0)" onClick={logoutSubmit}>
           <CIcon icon={cilLockLocked} className="me-2" />
           Logout
         </CDropdownItem>
